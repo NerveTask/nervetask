94d1ea97b86b1efca2cff1d818cbe7f98976e7b4
Patrick Daly <patrick@developdaly.com>
2014-02-17 08:13:18 -0600

    Stripped down JS

--- public/assets/js/public.js
+++ public/assets/js/public.js
@@ -22,60 +22,28 @@
 				action: 'nervetask_new_task',
 				url: nervetask.ajaxurl,
 				dataType: 'json',
-				beforeSend: function() {
-					console.log( data );
-				},
 				data: $(this).serialize()
 			})
 			.done(function( response ) {
 				if ( response ) {
 					try {
 
-						nervetask_updates( 'Success!', response );
 
 					} catch ( err ) {
-						nervetask_updates( 'Error', err );
+
 					}
 				}
 			})
 			.fail(function( response ) {
-				console.log( response );
 
-				nervetask_updates( 'fail', response );
 			})
 			.always(function( response ) {
-				console.log( 'complete' );
 			});
 
 			e.preventDefault();
 
 		});
 
-		$('#nervetask-updates').on('hidden.bs.modal', function (e) {
-
-			var title = '';
-			var body = '';
-
-			$('#nervetask-updates .modal-title').text( title );
-			$('#nervetask-updates .modal-body').html( body );
-
-		})
-
 	});
-
-	function nervetask_updates( status, response ) {
-
-		var modal = $('#nervetask-updates');
-		var title = '';
-		var body = '';
-
-		$(modal).modal('show');
-
-		title += status;
-		body += '<h2><a href="'+ response.post.guid +'">'+ response.post.post_title +'</a></h2>';
-
-		$('#nervetask-updates .modal-title').text( title );
-		$('#nervetask-updates .modal-body').html( body );
-	}
 
 }(jQuery));
