a2940c876108c1e436f22d182ebe704d6923566a
Patrick Daly <patrick@developdaly.com>
2014-03-16 23:24:47 -0500

    Remove Bootstrap-specific classes. Outfitted for chosen select

--- public/views/form-new-task.php
+++ public/views/form-new-task.php
@@ -1,12 +1,12 @@
 <?php if( is_user_logged_in() ) { ?>
 
-<form class="nervetask-new-task form-horizontal" role="form" method="post">
+<form class="nervetask-new-task" role="form" method="post">
 
 	<?php if( !isset( $atts['title'] ) || ( $atts['title'] == true ) ) { ?>
 
 	<div class="form-group">
-		<label class="control-label col-sm-2" for="nervetask-new-task-title">Title</label>
-		<div class="control-input col-sm-10">
+		<label class="control-label" for="nervetask-new-task-title">Title</label>
+		<div class="control-input">
 			<input type="text" class="form-control" id="nervetask-new-task-title" name="nervetask-new-task-title" placeholder="New task title...">
 		</div>
 	</div>
@@ -16,8 +16,8 @@
 	<?php if( !isset( $atts['content'] ) || ( $atts['content'] == true ) ) { ?>
 
 	<div class="form-group">
-		<label class="control-label col-sm-2" for="nervetask-new-task-content">Content</label>
-		<div class="control-input col-sm-10">
+		<label class="control-label" for="nervetask-new-task-content">Content</label>
+		<div class="control-input">
 			<textarea class="form-control" id="nervetask-new-task-content" name="nervetask-new-task-content"></textarea>
 		</div>
 	</div>
@@ -27,9 +27,9 @@
 	<?php if( isset( $atts['category']) && $atts['category'] == true ) { ?>
 
 	<div class="form-group">
-		<label class="control-label col-sm-2" for="nervetask-new-task-category">Categories</label>
-		<div class="control-input col-sm-10">
-			<select multiple="multiple" size="11" name="nervetask_category[]" class="form-control">
+		<label class="control-label" for="nervetask-new-task-category">Categories</label>
+		<div class="control-input">
+			<select multiple="multiple" size="11" name="nervetask_category[]" class="form-control chosen-select">
 
 			<?php
 				$categories = get_terms( 'nervetask_category', array( 'hide_empty' => 0, 'orderby' => 'slug' ) );
@@ -45,9 +45,9 @@
 	<?php if( isset( $atts['priority']) &&  $atts['priority'] == true ) { ?>
 
 	<div class="form-group">
-		<label class="control-label col-sm-2" for="nervetask-new-task-priority">Priority</label>
-		<div class="control-input col-sm-10">
-			<select multiple="multiple" size="11" name="nervetask_priority[]" class="form-control">
+		<label class="control-label" for="nervetask-new-task-priority">Priority</label>
+		<div class="control-input">
+			<select size="11" name="nervetask_priority[]" class="form-control chosen-select">
 
 			<?php
 				$priorities = get_terms( 'nervetask_priority', array( 'hide_empty' => 0, 'orderby' => 'slug' ) );
@@ -63,9 +63,9 @@
 	<?php if( isset( $atts['status']) &&  $atts['status'] == true ) { ?>
 
 	<div class="form-group">
-		<label class="control-label col-sm-2" for="nervetask-new-task-status">Status</label>
-		<div class="control-input col-sm-10">
-			<select multiple="multiple" size="11" name="nervetask_status[]" class="form-control">
+		<label class="control-label" for="nervetask-new-task-status">Status</label>
+		<div class="control-input">
+			<select size="11" name="nervetask_status[]" class="form-control chosen-select">
 
 			<?php
 				$statuses = get_terms( 'nervetask_status', array( 'hide_empty' => 0, 'orderby' => 'slug' ) );
@@ -81,8 +81,8 @@
 	<?php if( !isset( $atts['due_date'] ) || ( $atts['due_date'] == true ) ) { ?>
 
 	<div class="form-group">
-		<label class="control-label col-sm-2" for="nervetask-new-task-due-date">Due Date</label>
-		<div class="control-input col-sm-10">
+		<label class="control-label" for="nervetask-new-task-due-date">Due Date</label>
+		<div class="control-input">
 			<input type="text" class="form-control" id="nervetask-new-task-due-date-visible" name="nervetask-new-task-due-date-visible"></input>
 			<input type="hidden" id="nervetask-new-task-due-date" name="nervetask_due_date"></input>
 			<p id="nervetask-new-task-due-date-feedback" class="display-date"></p>
@@ -93,7 +93,7 @@
 
 
 	<div class="form-group">
-		<div class="col-sm-offset-2 col-sm-10">
+		<div class="col-sm-offset-2">
 			<button type="submit" class="btn">Create Task</button>
 		</div>
 	</div>
