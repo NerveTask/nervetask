045393149ac2c8c463bd6fd788bc7f512c483e19
Patrick Daly <patrick@developdaly.com>
2014-04-14 19:34:29 -0500

    Removed old comments. Formatting.

--- public/assets/js/due_date.js
+++ public/assets/js/due_date.js
@@ -1,37 +1,13 @@
-// due_date.js
-
 (function($, moment) {
-    // var storageFormat = "{yyyy}-{MM}-{dd} {hh}:{mm}:{ss}"
-    $(document).ready(function() {
-        var options = {
-            startDate: moment(),
-            singleDatePicker: true,
-            timePicker: true,
-            format: 'MM/DD/YYYY h:mm A'
-        };
-        $('#nervetask-new-task-due-date').daterangepicker(options);
-        $('#nervetask-update-task-due-date').daterangepicker(options);
+	$(document).ready(function() {
+		var options = {
+			startDate: moment(),
+			singleDatePicker: true,
+			timePicker: true,
+			format: 'MM/DD/YYYY h:mm A'
+		};
+		$('#nervetask-new-task-due-date').daterangepicker(options);
+		$('#nervetask-update-task-due-date').daterangepicker(options);
 		$('.daterangepicker').css('display', 'none');
-        // // var el = $('.nervetask-due-date');
-        // var input = $('#nervetask-new-task-due-date-visible');
-        // var output = $('#nervetask-new-task-due-date-feedback');
-        // var hidden = $('#nervetask-new-task-due-date');
-
-        // input.keyup(function() {
-        //     var val = input.val().trim();
-        //     if (/^\d+$/.test(val)) {
-        //         val = val.toNumber();
-        //     }
-        //     var text, date = Date.create(String(val));
-        //     if (!date.isValid()) {
-        //         text = 'Invalid date.'
-        //     } else {
-        //         text = date.full();
-        //         hidden.val(date.format(storageFormat));
-        //     }
-        //     output.text(text);
-        // });
-        // input.val('next friday at 5pm').keyup();
-    });
-
+	});
 })(jQuery, moment);
